5d62fb3e25834f63c408b35150ce90a4817949a4
pikarow <hu.rodriguez@gmx.de>
2017-11-19 15:43:40 +0100

    initial commit

--- src/Konto.java
+++ src/Konto.java
@@ -3,22 +3,24 @@ public class Konto {
     double kontostand;
 
 
-Konto (Person person, double kontostand){}
+Konto (Person person, double kontostand){
+    this.person= person; this.kontostand=kontostand
+}
 
 void einzahlen (double betrag ){
-    if (betrag <0 ){
-        System.out.println("Betrag konnte nicht eingezahlt werden, da keine negativen Zahlen eingezahlt werden können");
+    if (betrag < 0 ){
+        System.out.println("Betrag konnte nicht eingezahlt werden, da keine negativen Zahlen eingezahlt werden koennen");
     }
     else {
-    kontostand = kontostand+betrag;}
+    kontostand = kontostand + betrag;}
 
 }
 void auszahlen (double betrag){
     if ( betrag > kontostand){
-    System.out.println("Betrag konnte nicht abgehoben werden, da der kontostand zu gering ist.");
+    System.out.println("Betrag konnte nicht abgehoben werden, da der Kontostand zu gering ist.");
     }
     else{
-    kontostand = kontostand-betrag;}
+    kontostand = kontostand - betrag;}
 }
 
 }
