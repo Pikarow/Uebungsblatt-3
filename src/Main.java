96d0c824303588d88fbc33ef82865959c518334c
pikarow <hu.rodriguez@gmx.de>
2017-11-19 15:58:38 +0100

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(){
-        Person person = new Person( );
+        Person person = new Person("Manfred","Mustermann",32,"Musterstrasse vier");
     }
-
+Konto kundenkonto = new Konto(Person, 2000);
 
 }
