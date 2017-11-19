5dc992851a977c58e25e853bebb35d8e292d42bf
pikarow <hu.rodriguez@gmx.de>
2017-11-19 15:46:20 +0100

    initial commit

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,13 @@
 public class Person{
-    String vormane;
+    String vorname;
     String nachname;
     int alter;
     String adresse;
+
+    public Person(String vorname, String nachname, int alter, String adresse) {
+        this.vorname = vorname;
+        this.nachname = nachname;
+        this.alter = alter;
+        this.adresse = adresse;
+    }
 }
