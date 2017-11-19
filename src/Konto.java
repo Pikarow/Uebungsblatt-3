1425fc009e7f74eb33ef5fa0baf82ae13b1df1c9
pikarow <hu.rodriguez@gmx.de>
2017-11-19 15:29:39 +0100

    initial commit

--- src/Konto.java
+++ src/Konto.java
@@ -6,13 +6,13 @@ public class Konto {
 Konto (Person person, double kontostand){}
 
 void einzahlen (double betrag ){
-    kontostand=kontostand+betrag;
+    kontostand = kontostand+betrag;
 }
 void auszahlen (double betrag){
-    kontostand=kontostand-betrag;
     if ( betrag > kontostand){
-    System.out.println(Betrag konnte nicht abgehoben)
+    System.out.println("Betrag konnte nicht abgehoben werden, da der kontostand zu gering ist.");
     }
+    kontostand = kontostand-betrag;
 }
 
 }
